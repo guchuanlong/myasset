3d59d6951b036ec42abfe057282b985f59939356
guchuanlong <gucl@asiainfo.com>
2019-01-03 23:19:32 +0800

    190102

--- src/main/java/com/thinkgem/jeesite/modules/myasset/service/BusiAssetLibinBillService.java
+++ src/main/java/com/thinkgem/jeesite/modules/myasset/service/BusiAssetLibinBillService.java
@@ -5,13 +5,16 @@ package com.thinkgem.jeesite.modules.myasset.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.thinkgem.jeesite.common.persistence.Page;
 import com.thinkgem.jeesite.common.service.CrudService;
-import com.thinkgem.jeesite.modules.myasset.entity.BusiAssetLibinBill;
 import com.thinkgem.jeesite.modules.myasset.dao.BusiAssetLibinBillDao;
+import com.thinkgem.jeesite.modules.myasset.dao.BusiAssetMainDao;
+import com.thinkgem.jeesite.modules.myasset.entity.BusiAssetLibinBill;
+import com.thinkgem.jeesite.modules.myasset.entity.BusiAssetMain;
 
 /**
  * 资产入库Service
@@ -22,6 +25,9 @@ import com.thinkgem.jeesite.modules.myasset.dao.BusiAssetLibinBillDao;
 @Transactional(readOnly = true)
 public class BusiAssetLibinBillService extends CrudService<BusiAssetLibinBillDao, BusiAssetLibinBill> {
 
+	@Autowired
+	private BusiAssetMainDao assetMainDao;
+	
 	public BusiAssetLibinBill get(String id) {
 		return super.get(id);
 	}
@@ -36,7 +42,26 @@ public class BusiAssetLibinBillService extends CrudService<BusiAssetLibinBillDao
 	
 	@Transactional(readOnly = false)
 	public void save(BusiAssetLibinBill busiAssetLibinBill) {
-		super.save(busiAssetLibinBill);
+		//super.save(busiAssetLibinBill);
+		if (busiAssetLibinBill.getIsNewRecord()){
+			busiAssetLibinBill.preInsert();
+			dao.insert(busiAssetLibinBill);
+			//插入到资产主表
+			int libnum=Integer.parseInt(busiAssetLibinBill.getLibinNum());
+			for(int i=0;i<libnum;i++) {
+				BusiAssetMain am=new BusiAssetMain();
+				//TODO 设置资产属性
+				am.preInsert();
+				assetMainDao.insert(am);
+			}
+			
+			//TODO 插入到库存表
+			
+			
+		}else{
+			busiAssetLibinBill.preUpdate();
+			dao.update(busiAssetLibinBill);
+		}
 	}
 	
 	@Transactional(readOnly = false)
